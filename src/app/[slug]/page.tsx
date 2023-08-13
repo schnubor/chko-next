5edb683c2ea65a78ba5fd6d05639d875579860bd
Christian Korndoerfer <christian@perspective.co>
2023-08-14 00:41:12 +0200

    hygraph working

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,12 +1,56 @@
 import { notFound } from 'next/navigation';
 import { ALLOWED_PAGES } from '../constants';
+import { GraphQLClient } from 'graphql-request';
 
-const Perspective = ({ params }: { params: { slug: string } }) => {
-    if (!ALLOWED_PAGES.includes(params.slug)) {
-        notFound()
+const hygraph = new GraphQLClient(
+    process.env.NEXT_PUBLIC_HYGRAPH_API as string
+);
+
+interface Work {
+    headline: string;
+    content: string;
+    icon: {
+        url: string;
     }
-
-    return <h1>{params.slug}</h1>
 }
 
-export default Perspective;
+const fetchWork = async (slug: string) => {
+    const { work } = await hygraph.request<{ work: Work }>(
+        `query WorkPageQuery($slug: String!) {
+          work(where: { slug: $slug }) {
+            headline
+            content
+            icon {
+                url
+            }
+          }
+        }`,
+        {
+            slug,
+        }
+    );
+
+    return work;
+}
+
+const WorkPage = async ({ params }: { params: { slug: string } }) => {
+    if (!ALLOWED_PAGES.includes(params.slug)) {
+        return notFound();
+    }
+
+    const work = await fetchWork(params.slug);
+
+    if (!work) {
+        return notFound();
+    }
+
+    return (
+        <>
+            <h1>{work.headline}</h1>
+            <img src={work.icon.url} />
+            <p>{work.content}</p>
+        </>
+    );
+}
+
+export default WorkPage;
