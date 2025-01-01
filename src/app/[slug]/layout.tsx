b1cf88a4fb1d892a86ee52f41b67ca1be85b1099
Christian Korndoerfer <christian@perspective.co>
2025-01-01 23:59:31 +0100

    fix build

--- src/app/[slug]/layout.tsx
+++ src/app/[slug]/layout.tsx
@@ -1,6 +1,7 @@
 // UI
 import { ArrowLeftIcon } from '@radix-ui/react-icons';
 import { Header } from '../components/Header';
+import Link from 'next/link';
 
 // Types
 import type { FC, ReactNode } from 'react';
@@ -12,14 +13,14 @@ interface Props {
 const Layout: FC<Props> = ({ children }) => (
     <main className="mt-12">
         <Header>
-            <a href="/">
+            <Link href="/">
                 <button className="group flex items-center py-2 pr-2 text-sm text-stone-500 transition hover:text-stone-900 dark:text-neutral-400 dark:hover:text-neutral-300">
                     <ArrowLeftIcon className="h-5 w-5 transition-all group-hover:text-orange-500" />
                     <span className="ml-2 font-semibold transition-all duration-500 ease-out group-hover:ml-3">
                         Home
                     </span>
                 </button>
-            </a>
+            </Link>
         </Header>
 
         <div className="mt-8">{children}</div>
