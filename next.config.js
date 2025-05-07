7af85c2a4a658c2858351c7e225964593d4747f3
Christian Korndoerfer <christian@perspective.co>
2025-05-07 22:56:12 +0200

    enable react compiler

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['localhost', 'media.graphassets.com'],
     },
+    experimental: {
+        reactCompiler: true,
+    },
 };
 
 module.exports = nextConfig;
