6dd07dc3bcb4e81bd2ccea15381959f0c7aeef67
Christian Korndoerfer <christian@perspective.co>
2025-11-15 21:19:54 +0100

    upgrade to next16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,114 +1,150 @@
-import { defineConfig } from "eslint/config";
-import unusedImports from "eslint-plugin-unused-imports";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import stylistic from "@stylistic/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import { defineConfig } from 'eslint/config';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
-export default defineConfig([{
-    extends: compat.extends("eslint:recommended", "next/core-web-vitals", "prettier"),
+export default defineConfig([
+    {
+        extends: [
+            ...compat.extends('eslint:recommended'),
+            ...nextCoreWebVitals,
+            ...compat.extends('prettier'),
+        ],
 
-    plugins: {
-        "unused-imports": unusedImports,
-        "@typescript-eslint": typescriptEslint,
-        "@stylistic": stylistic,
-    },
-
-    languageOptions: {
-        globals: {
-            NodeJS: true,
-            JSX: "readonly",
+        plugins: {
+            'unused-imports': unusedImports,
+            '@typescript-eslint': typescriptEslint,
+            '@stylistic': stylistic,
         },
 
-        parser: tsParser,
-    },
+        languageOptions: {
+            globals: {
+                NodeJS: true,
+                JSX: 'readonly',
+            },
 
-    rules: {
-        "no-unused-vars": "off",
+            parser: tsParser,
+        },
 
-        quotes: ["warn", "single", {
-            avoidEscape: true,
-        }],
+        rules: {
+            'no-unused-vars': 'off',
 
-        curly: "warn",
-        "@typescript-eslint/consistent-type-imports": "warn",
-        "@typescript-eslint/no-unused-vars": ["error"],
-        "newline-before-return": "warn",
-        "no-use-before-define": ["error", "nofunc"],
-        "unused-imports/no-unused-imports": "error",
-
-        "react/jsx-curly-brace-presence": ["warn", {
-            props: "never",
-            children: "never",
-        }],
-
-        "prefer-arrow-callback": ["error", {
-            allowNamedFunctions: true,
-        }],
-
-        "func-style": ["error", "expression", {
-            allowArrowFunctions: true,
-        }],
-
-        "@stylistic/padding-line-between-statements": ["error", {
-            blankLine: "always",
-            prev: "*",
-            next: "if",
-        }, {
-            blankLine: "always",
-            prev: "*",
-            next: "try",
-        }, {
-            blankLine: "always",
-            prev: "*",
-            next: "switch",
-        }],
-
-        "import/order": ["error", {
-            "newlines-between": "always",
-
-            groups: [
-                "unknown",
-                "builtin",
-                "external",
-                "internal",
-                ["sibling", "parent"],
-                "object",
-                "index",
-                "type",
+            quotes: [
+                'warn',
+                'single',
+                {
+                    avoidEscape: true,
+                },
             ],
 
-            pathGroups: [{
-                pattern: "**/constants",
-                group: "unknown",
-                position: "before",
-            }, {
-                pattern: "@/core/redux/store",
-                group: "internal",
-                position: "before",
-            }],
+            curly: 'warn',
+            '@typescript-eslint/consistent-type-imports': 'warn',
+            '@typescript-eslint/no-unused-vars': ['error'],
+            'newline-before-return': 'warn',
+            'no-use-before-define': ['error', 'nofunc'],
+            'unused-imports/no-unused-imports': 'error',
 
-            alphabetize: {
-                order: "asc",
-                caseInsensitive: true,
-            },
-        }],
+            'react/jsx-curly-brace-presence': [
+                'warn',
+                {
+                    props: 'never',
+                    children: 'never',
+                },
+            ],
 
-        "id-length": ["error", {
-            min: 2,
-            exceptions: ["_", "t", "i"],
-            properties: "never",
-        }],
+            'prefer-arrow-callback': [
+                'error',
+                {
+                    allowNamedFunctions: true,
+                },
+            ],
+
+            'func-style': [
+                'error',
+                'expression',
+                {
+                    allowArrowFunctions: true,
+                },
+            ],
+
+            '@stylistic/padding-line-between-statements': [
+                'error',
+                {
+                    blankLine: 'always',
+                    prev: '*',
+                    next: 'if',
+                },
+                {
+                    blankLine: 'always',
+                    prev: '*',
+                    next: 'try',
+                },
+                {
+                    blankLine: 'always',
+                    prev: '*',
+                    next: 'switch',
+                },
+            ],
+
+            'import/order': [
+                'error',
+                {
+                    'newlines-between': 'always',
+
+                    groups: [
+                        'unknown',
+                        'builtin',
+                        'external',
+                        'internal',
+                        ['sibling', 'parent'],
+                        'object',
+                        'index',
+                        'type',
+                    ],
+
+                    pathGroups: [
+                        {
+                            pattern: '**/constants',
+                            group: 'unknown',
+                            position: 'before',
+                        },
+                        {
+                            pattern: '@/core/redux/store',
+                            group: 'internal',
+                            position: 'before',
+                        },
+                    ],
+
+                    alphabetize: {
+                        order: 'asc',
+                        caseInsensitive: true,
+                    },
+                },
+            ],
+
+            'id-length': [
+                'error',
+                {
+                    min: 2,
+                    exceptions: ['_', 't', 'i'],
+                    properties: 'never',
+                },
+            ],
+        },
     },
-}]);
+]);
